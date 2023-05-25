fe2148c05e00d6217b856522882e730a679622d7
elvis <xenixenito@gmail.com>
2023-05-25 08:05:02 +0200

    creacion de model, registered y services

--- sudoku/src/app/app.module.ts
+++ sudoku/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './public/landing-page/landing-page.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarPublicComponent } from './public/navbar-public/navbar-public.component';
+import { SudokuComponent } from './sudoku/sudoku/sudoku.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavbarPublicComponent,
+    SudokuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
